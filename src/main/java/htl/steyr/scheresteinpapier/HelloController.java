a3c6afc834be28a7b632bfdc25ce4c7a8b937155
Maximilian Leimhofer <mleimhof@htl-steyr.ac.at>
2025-11-29 12:53:29 +0100

    fxml modified

--- src/main/java/htl/steyr/scheresteinpapier/HelloController.java
+++ src/main/java/htl/steyr/scheresteinpapier/HelloController.java
@@ -1,14 +1,29 @@
 package htl.steyr.scheresteinpapier;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
 
 public class HelloController {
+    public ProgressBar pBar;
+    public TextArea playerBox2;
+    public TextArea playerBox1;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onScissorsClicked(ActionEvent actionEvent) {
+    }
+
+    public void onRockClicked(ActionEvent actionEvent) {
+    }
+
+    public void onPaperClicked(ActionEvent actionEvent) {
     }
 }
