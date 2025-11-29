78191db37b54fdbca92c91afdf43d89b4cc34c86
Sena <skara1@htl-steyr.ac.at>
2025-11-29 10:53:15 +0100

    Basis

--- src/main/java/htl/steyr/scheresteinpapier/HelloApplication.java
+++ src/main/java/htl/steyr/scheresteinpapier/HelloApplication.java
@@ -11,7 +11,6 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        //Teständerungen
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
