26523d00ab1e0d6dd55e2f17d996cf3630905b59
Sena <skara1@htl-steyr.ac.at>
2025-11-30 14:23:52 +0100

    done coding

--- src/main/java/htl/steyr/scheresteinpapier/HelloController.java
+++ src/main/java/htl/steyr/scheresteinpapier/HelloController.java
@@ -5,25 +5,38 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Random;
 
 public class HelloController {
     public ProgressBar pBar;
-    public TextArea playerBox2;
-    public TextArea playerBox1;
-    @FXML
-    private Label welcomeText;
+    public ImageView playerBox1; //Spieler Bild
+    public ImageView playerBox2; //Computer Bild
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private final Random rand = new Random();
+    private String computer;
 
     public void onScissorsClicked(ActionEvent actionEvent) {
+        playerBox1.setImage(new Image(String.valueOf(getClass().getResource("/images/schere.png"))));
+        mainProcess();
     }
 
     public void onRockClicked(ActionEvent actionEvent) {
+        playerBox1.setImage(new Image(String.valueOf(getClass().getResource("/images/stein.png"))));
+        mainProcess();
     }
 
     public void onPaperClicked(ActionEvent actionEvent) {
+        playerBox1.setImage(new Image(String.valueOf(getClass().getResource("/images/papier.png"))));
+        mainProcess();
     }
+
+    public void mainProcess() {
+        String[] wahl = {"schere.png", "stein.png", "papier.png"};
+        computer = wahl[rand.nextInt(3)];
+        playerBox2.setImage(new Image(String.valueOf(getClass().getResource("/images/" + computer))));
+    }
+
 }
